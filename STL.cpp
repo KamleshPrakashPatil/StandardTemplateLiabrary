3baead4d28351a9f64192b78e1f62215ba4426a8
Kamlesh Prakash Patil <99859364+KamleshPrakashPatil@users.noreply.github.com>
2022-07-09 14:37:35 +0530

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -703,3 +703,177 @@ int DoublyCLL<T>::Count()
 {
     return CountNode;
 }
+template<class T>
+struct node
+{
+    T data;
+    struct node *next;
+};
+
+template<class T>
+class Stack
+{
+    public:
+        struct node<T> *Head;
+        int Count;
+
+        Stack();
+        void Push(T);    //InerstFirst
+        void Pop();         //DeleteFirst
+        void Display();
+        int CountNode();
+};
+
+template<class T>
+Stack<T>::Stack()
+{
+    Head = NULL;
+    Count = 0;
+}
+
+template<class T>
+void Stack<T>::Push(T no)
+{
+    struct node<T> *newn = NULL;
+    newn = new node<T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    
+    newn->next=Head;
+    Head=newn;
+
+    Count++;
+}
+
+template<class T>
+void Stack <T>::Pop()       //DeleteFirst();
+{
+    T no;
+
+    if(Count==0)
+    {
+        cout<<"Stack is empty"<<endl;
+        return;
+    }
+
+    no=Head->data;
+    struct node <T> *temp=Head;
+    Head=Head->next;
+    delete temp;
+
+    Count--;
+    cout<<"Removed element is: "<<no<<endl;
+}
+
+template<class T>
+void Stack<T>::Display()
+{
+    cout<<"Elements from Stack are : "<<endl;
+    struct node<T> *temp = Head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+template<class T>
+int Stack<T>::CountNode()
+{
+    return Count;
+}
+template<class T>
+struct node
+{
+    T data;
+    struct node *next;
+};
+
+template<class T>
+class Queue
+{
+    public:
+        struct node<T> *Head;
+        int Count;
+
+        Queue();
+        void Enqueue(T);    //InerstLast
+        void Dqueue();         //DeleteFirst
+        void Display();
+        int CountNode();
+};
+
+template<class T>
+Queue<T>::Queue()
+{
+    Head = NULL;
+    Count = 0;
+}
+
+template<class T>
+void Queue<T>::Enqueue(T no)
+{
+    struct node<T> *newn = NULL;
+    newn = new node<T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(Head == NULL)
+    {
+        Head = newn;
+    }
+    else
+    {
+            struct node<T> *temp = Head;
+            while(temp->next != NULL)
+            {
+                temp = temp -> next;
+            }
+
+            temp->next = newn;
+    }
+    Count++;
+}
+
+template<class T>
+void Queue <T>::Dqueue()       //DeleteFirst();
+{
+    T no;
+
+    if(Count==0)
+    {
+        cout<<"Queue is empty"<<endl;
+        return;
+    }
+
+    no=Head->data;
+    struct node <T> *temp=Head;
+    Head=Head->next;
+    delete temp;
+
+    Count--;
+    cout<<"Removed element is: "<<no<<endl;
+}
+
+template<class T>
+void Queue<T>::Display()
+{
+    cout<<"Elements from Queue are : "<<endl;
+    struct node<T> *temp = Head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+template<class T>
+int Queue<T>::CountNode()
+{
+    return Count;
+}
